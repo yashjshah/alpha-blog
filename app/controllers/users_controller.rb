a5508f94c33aafb4ebeb4c2bdf6d3b8ab3bf4ae0
Yash <yashjshah@yahoo.com>
2018-02-06 21:37:37 +0000

    Adding restrictions to the users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only: [:edit, :update, :show]
   def index
     @users = User.paginate(page: params[:page], per_page: 5) 
   end
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
   
   def create
    @user = User.new(user_params)
-
     if @user.save
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
       redirect_to articles_path
@@ -20,11 +19,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated succesfully "
       redirect_to articles_path
@@ -34,13 +31,16 @@ class UsersController < ApplicationController
   end
   
   def show 
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5) 
   end
   
   private 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+  
+  def set_user
+     @user = User.find(params[:id])
   end
   
 end
