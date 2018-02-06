c3223a0b5b6bb27eca15830e9acef1982f58fe2b
Yash <yashjshah@yahoo.com>
2018-02-06 20:29:14 +0000

    Create and destroy sessions and add login page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ get 'about', to: 'pages#about'
 
 get 'signup', to: 'users#new'
 
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
+
+
 resources :users, except: [:new]
 
 resources :articles
