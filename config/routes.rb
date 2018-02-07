491054aefff32e60cee2e2e0dda9e71620c9e8a9
Yash <yashjshah@yahoo.com>
2018-02-07 20:15:29 +0000

    Add categories model and test

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
 
-
+resources :categories, except: [:destroy]
 resources :users, except: [:new]
 
 resources :articles
